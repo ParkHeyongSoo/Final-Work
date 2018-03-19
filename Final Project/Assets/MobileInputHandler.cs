181cd234bffabcae949af57a300ae9336400ddb3
Park Hyeong Soo <parkgudtn@naver.com>
2018-03-19 23:51:27 +0900

    first comment
    Added unity project to Github

---
+++ Final Project/Assets/MobileInputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInputHandler : MonoBehaviour {
+
+
+    public delegate void MyInput(float x, float y);
+    public static event MyInput Touch;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Input.mousePosition.ToString());
+            Touch(Input.mousePosition.x, Input.mousePosition.y);
+           
+        }
+	}
+}
---
+++ Final Project/Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public bool bStart;
+
+    private float _Speed;
+    public float _Accel;
+    public float JumpPower;
+
+	// Use this for initialization
+	void Awake () {
+        _Speed = 0f;
+        bStart = false;
+        MobileInputHandler.Touch += Jump;
+        MobileInputHandler.Touch += TouchToStart;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Physics();
+
+    }
+
+    void TouchToStart(float x, float y)
+    {
+        if (bStart)
+            return;
+
+        bStart = true;
+    }
+
+    void Physics()
+    {
+        if (!bStart)
+            return;
+        _Speed -= _Accel * Time.deltaTime;
+
+        transform.Translate(0f, _Speed * Time.deltaTime, 0f, Space.World);
+    }
+
+    void Jump(float x, float y)
+    {
+        if (!bStart)
+            return;
+
+        _Speed += JumpPower;
+    }
+}
